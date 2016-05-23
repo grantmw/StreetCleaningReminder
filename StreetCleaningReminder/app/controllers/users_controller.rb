97a018a3f84f1c747a252621ced3125a5adf73f7
grantmw <grantmwong@gmail.com>
2016-05-23 15:52:24 -0700

    added users resources,added create route

--- StreetCleaningReminder/app/controllers/users_controller.rb
+++ StreetCleaningReminder/app/controllers/users_controller.rb
@@ -1,3 +1,6 @@
+require 'rest_client'
+require 'json'
+
 class UsersController < ApplicationController
   def index
   end
@@ -6,6 +9,16 @@ class UsersController < ApplicationController
   end
 
   def create
+    p "8" * 100
+    p params
+    p "8" * 100
+    user = User.new(first_name: params['email'], password: params['password'], phone_number: params['phone_number'])
+    if user.save
+      #session["id"] = user.id
+      render json: user, status: :created
+    else
+      render nothing: true, status: 406
+    end
   end
 
   def show
