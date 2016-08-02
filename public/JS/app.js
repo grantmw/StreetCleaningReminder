1a8dcdcbb1c154bbddc061d453950459ea3edf6a
grantmw <grantmwong@gmail.com>
2016-08-02 10:13:27 -0700

    angular ng route cdn fix

--- public/JS/app.js
+++ public/JS/app.js
@@ -1,30 +1,13 @@
+
+
 var app = angular.module('app', ['ngRoute', 'ngCookies']);
 
 app.config(function ($routeProvider, $locationProvider){ $routeProvider
-	// .when('/',{
-	// 	controller: 'SigninController',
-	// 	templateUrl: '../components/reminders.html'
-	// })
+
 	.when('/',{
 		controller: 'RemindersController',
 		templateUrl: '../components/reminders.html'
 	})
 
-
-
 })
 
-// app.service('shareVariables', function () {
-//         var property = {
-//         	data: 'test object value'
-//         };
-
-//         return {
-//             getProperty: function () {
-//                 return property;
-//             },
-//             setProperty: function(value) {
-//                 property = value;
-//         }
-//     };
-// });
