d10e2836c2078645bf480f3273bd21f00a312ad9
grantmw <grantmwong@gmail.com>
2016-08-02 00:16:18 -0700

    using config.yml instead of secrets.yml

--- config/environment.rb
+++ config/environment.rb
@@ -4,7 +4,7 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 Rails.application.initialize!
 
+#uncomment to use secrets.yml
+#APP_CONFIG = YAML.load_file("#{Rails.root}/config/secrets.yml")
 
-APP_CONFIG = YAML.load_file("#{Rails.root}/config/secrets.yml")
-
-#APP_CONFIG = YAML.load_file("#{RAILS_ROOT}/config/config.yml")
+APP_CONFIG = YAML.load_file("#{RAILS_ROOT}/config/config.yml")
