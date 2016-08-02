65e9ffb666837818b6d0f6961c71ce178ba173ef
grantmw <grantmwong@gmail.com>
2016-08-02 11:49:02 -0700

    angular config minify dependency error fix

--- public/JS/app.js
+++ public/JS/app.js
@@ -2,13 +2,15 @@
 
 var app = angular.module('app', ['ngRoute', 'ngCookies']);
 
-app.config(function ($routeProvider, $locationProvider){ 
+app.config(['$routeProvider', '$locationProvider', 
+	function ($routeProvider, $locationProvider){ 
 
-	$routeProvider
-	.when('/',{
-		controller: 'RemindersController',
-		templateUrl: '../components/reminders.html'
-	})
+		$routeProvider
+		.when('/',{
+			controller: 'RemindersController',
+			templateUrl: '../components/reminders.html'
+		})
 
-})
+	}
+]);
 
