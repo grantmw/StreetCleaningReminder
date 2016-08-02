929a137c613e755fda907e64b8137443ebc91ace
grantmw <grantmwong@gmail.com>
2016-08-02 11:31:32 -0700

    absolute url

--- public/JS/app.js
+++ public/JS/app.js
@@ -2,8 +2,9 @@
 
 var app = angular.module('app', ['ngRoute', 'ngCookies']);
 
-app.config(function ($routeProvider, $locationProvider){ $routeProvider
+app.config(function ($routeProvider, $locationProvider){ 
 
+	$routeProvider
 	.when('/',{
 		controller: 'RemindersController',
 		templateUrl: '../components/reminders.html'
