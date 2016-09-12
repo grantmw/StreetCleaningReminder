13a5915d823498b59c62ef35796983556be669ed
grantmw <grantmwong@gmail.com>
2016-09-11 21:40:11 -0700

    semicolon, white space

--- public/js/app.js
+++ public/js/app.js
@@ -1,143 +1,123 @@
-var app = angular.module('app', ['ngRoute', 'ngCookies']);
-app.config(['$routeProvider', '$locationProvider', 
+var app = angular.module("app", ["ngRoute", "ngCookies"]);
+app.config(["$routeProvider", "$locationProvider", 
 	function ($routeProvider, $locationProvider){ 
 		$routeProvider
-		.when('/',{
-			controller: 'RemindersController',
-			templateUrl: '../components/reminders.html'
-		})
+		.when("/",{
+			controller: "RemindersController",
+			templateUrl: "../components/reminders.html"
+		});
 	}
 ]);
 
-app.controller('RemindersController', ['$scope', '$http', '$cookies', function($scope, $http, $cookies){
+app.controller("RemindersController", ["$scope", "$http", "$cookies", function($scope, $http, $cookies){
 
-	$scope.reminders = []
-	$scope.hourAndDuration = ""
-	$scope.day = ""
-	$scope.frequency = ""
-	$scope.phoneNumber = ""
-	$scope.password = ""
-	$scope.userName = ""
-	$scope.reminderName = ""
-
-
-
+	$scope.reminders = [];
+	$scope.hourAndDuration = "";
+	$scope.day = "";
+	$scope.frequency = "";
+	$scope.phoneNumber = "";
+	$scope.password = "";
+	$scope.userName = "";
+	$scope.reminderName = "";
 
 	var check_login = function(){	
-		if ($cookies.get('loggedin') == 'false' || typeof $cookies.get('loggedin') == 'undefined'){
-			$(".not-logged-in").show()
-			$('.show_reminders').hide()
-			$(".logged-in").hide()
+		if ($cookies.get("loggedin") == "false" || typeof $cookies.get("loggedin") == "undefined"){
+			$(".not-logged-in").show();
+			$(".show_reminders").hide();
+			$(".logged-in").hide();
 		}
 		else {
-			$(".logged-in").show()
-			$(".not-logged-in").hide()
-			$('.welcome-message').html('Logged in as: ' + $cookies.get('user_name'))
+			$(".logged-in").show();
+			$(".not-logged-in").hide();
+			$(".welcome-message").html("Logged in as: " + $cookies.get("user_name"));
 		}
-	}
-	check_login()
+	};
+	check_login();
 
 	var get_reminders = function(){
 		$http({
-			url: '/reminders',
-			method: 'GET',
-			params: {user_phone_number: $cookies.get('userPhoneNumber')}
+			url: "/reminders",
+			method: "GET",
+			params: {user_phone_number: $cookies.get("userPhoneNumber")}
 		}).success(function(response){
-			$scope.reminders = response['reminders']
+			$scope.reminders = response["reminders"];
 			if($scope.reminders.length == 0){
-				$(".show_reminders").hide()
-				if ($cookies.get('loggedin') == 'true'){
-					$(".no_reminders").show()	
+				$(".show_reminders").hide();
+				if ($cookies.get("loggedin") == "true"){
+					$(".no_reminders").show();
 				}
 			} else {
-				$(".show_reminders").show()
-				$(".no_reminders").hide()
+				$(".show_reminders").show();
+				$(".no_reminders").hide();
 			}
-			$scope.phoneNumber = $cookies.get('userPhoneNumber')
+			$scope.phoneNumber = $cookies.get("userPhoneNumber");
 		})
 		.error(function(response){
-			console.log("Failed to get reminders")
-		})
+		});
+	};
 
-
-	}
-	if ($cookies.get('loggedin') == 'true'){
-		get_reminders()
+	if ($cookies.get("loggedin") == "true"){
+		get_reminders();
 	}
 
 	$scope.delete_reminder = function(id){
-		console.log("this is the id: " + String(id))
-
 		$http({
 			url: "/reminders/" + id,
-			method: 'DELETE',
+			method: "DELETE",
 		}).success(function(response){
-			console.log("reminder successfully deleted")
 			get_reminders()
 		})
 		.error(function(response){
-			console.log("Failed to delete reminder")
-		})
-
+		});
 	}
 
 	$scope.create_reminder = function(){
-		console.log("this is the create reminder function: this is scope phone_number")
-		console.log($scope.phoneNumber)
-
-
-
 		var reminder_attr = {
 			"hourAndDuration": $scope.hourAndDuration,
 			"day": $scope.day,
 			"frequency": $scope.frequency,
-			"phone_number": $cookies.get('userPhoneNumber'),
+			"phone_number": $cookies.get("userPhoneNumber"),
 			"reminder_name": $scope.reminderName
-		}
+		};
 
-		$http.post('/reminders', reminder_attr).success(function(response){
-			get_reminders()
-			$('form').each(function() { this.reset() })
-			$('select').each(function() { this.selectedIndex = -1 })
+		$http.post("/reminders", reminder_attr).success(function(response){
+			get_reminders();
+			$("form").each(function() { this.reset() });
+			$("select").each(function() { this.selectedIndex = -1 });
 		})
 		.error(function(response){
-			console.log("Failed to create_reminder")
-			console.log(response[0])
-			if ($cookies.get('userPhoneNumber') == 'not logged in'){
-				alert("Login to create a reminder")
+			if ($cookies.get("userPhoneNumber") == "not logged in"){
+				alert("Login to create a reminder");
 			} else{
-				alert(response[0])			
+				alert(response[0]);	
 			}
-		})
+		});
 	}
-
 
 	$scope.signin = function(){
 		var user = {
 			"phone_number": $scope.phoneNumber,
 			"password": $scope.password 
-		}
+		};
 
-
-		$http.post('/sessions', user).success(function(response){
-			$cookies.put('loggedin', 'true')
-			$cookies.put('user_name', response['user_name'])
-			$scope.reminders = response['reminders']
-			$scope.userName = response['user_name']
-			$scope.phoneNumber = String(response['user_phone_number'])
-			$cookies.put('userPhoneNumber', String(response['user_phone_number']))
-			$('.welcome_message').html("Logged in as: " + $scope.userName)
+		$http.post("/sessions", user).success(function(response){
+			$cookies.put("loggedin", "true");
+			$cookies.put("user_name", response["user_name"]);
+			$scope.reminders = response["reminders"];
+			$scope.userName = response["user_name"];
+			$scope.phoneNumber = String(response["user_phone_number"]);
+			$cookies.put("userPhoneNumber", String(response["user_phone_number"]));
+			$(".welcome_message").html("Logged in as: " + $scope.userName);
 			check_login();
 			get_reminders();
 			window.location.reload();
 		})
 		.error(function(response){
-			console.log("Failed")
-			$cookies.put('loggedin', 'false')
-			$cookies.put('userPhoneNumber', 'not logged in')
-			$('.signin-error h5').html("Incorrect phone number or password")
-			$('.signin-error').show()
-			$("#myModal").modal("toggle")
+			$cookies.put("loggedin", "false");
+			$cookies.put("userPhoneNumber", "not logged in");
+			$(".signin-error h5").html("Incorrect phone number or password");
+			$(".signin-error").show();
+			$("#myModal").modal("toggle");
 		})
 	}	
 
@@ -146,20 +126,16 @@ app.controller('RemindersController', ['$scope', '$http', '$cookies', function($
 			"phone_number": $scope.phoneNumber,
 			"password": $scope.password,
 			"user_name": $scope.userName
-		}
+		};
 
-		$http.post('/users', user).success(function(response){
-			console.log("Successfully Registered")
+		$http.post("/users", user).success(function(response){
 			window.location.reload();
 		})
 		.error(function(response){
-			console.log("Failed to Register")
-			console.log(response)
-			console.log(response[0])
-			$('.register-error h5').html(response[0])
-			$('.register-error').show()
-			$("#myRegisterModal").modal("toggle")
-		})
+			$(".register-error h5").html(response[0]);
+			$(".register-error").show();
+			$("#myRegisterModal").modal("toggle");
+		});
 	}
 
 	$scope.closeModal = function(){
@@ -168,11 +144,11 @@ app.controller('RemindersController', ['$scope', '$http', '$cookies', function($
 	}
 
 	$scope.log_out = function(){
-		$cookies.remove('loggedin')
-		$cookies.remove('userPhoneNumber')
-		$cookies.remove('user_name')
-		$scope.userName = ""
-		$('.welcome_message').css("display", "none")
-		check_login()
+		$cookies.remove("loggedin");
+		$cookies.remove("userPhoneNumber");
+		$cookies.remove("user_name");
+		$scope.userName = "";
+		$(".welcome_message").css("display", "none");
+		check_login();
 	}
 }])
