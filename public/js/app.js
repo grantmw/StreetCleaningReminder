2571f0952483f44f5b9c9033853d49ad9eea5763
grantmw <grantmwong@gmail.com>
2016-09-11 21:11:21 -0700

    js camel case conversion & no reminder info alert

--- public/js/app.js
+++ public/js/app.js
@@ -12,13 +12,13 @@ app.config(['$routeProvider', '$locationProvider',
 app.controller('RemindersController', ['$scope', '$http', '$cookies', function($scope, $http, $cookies){
 
 	$scope.reminders = []
-	$scope.hourAndDuration = ''
-	$scope.day = ''
-	$scope.frequency = ''
-	$scope.phone_number = ""
+	$scope.hourAndDuration = ""
+	$scope.day = ""
+	$scope.frequency = ""
+	$scope.phoneNumber = ""
 	$scope.password = ""
-	$scope.user_name = ""
-	$scope.reminder_name = ""
+	$scope.userName = ""
+	$scope.reminderName = ""
 
 
 
@@ -41,7 +41,7 @@ app.controller('RemindersController', ['$scope', '$http', '$cookies', function($
 		$http({
 			url: '/reminders',
 			method: 'GET',
-			params: {user_phone_number: $cookies.get('user_phone_number')}
+			params: {user_phone_number: $cookies.get('userPhoneNumber')}
 		}).success(function(response){
 			$scope.reminders = response['reminders']
 			if($scope.reminders.length == 0){
@@ -53,7 +53,7 @@ app.controller('RemindersController', ['$scope', '$http', '$cookies', function($
 				$(".show_reminders").show()
 				$(".no_reminders").hide()
 			}
-			$scope.phone_number = $cookies.get('user_phone_number')
+			$scope.phoneNumber = $cookies.get('userPhoneNumber')
 		})
 		.error(function(response){
 			console.log("Failed to get reminders")
@@ -83,13 +83,16 @@ app.controller('RemindersController', ['$scope', '$http', '$cookies', function($
 
 	$scope.create_reminder = function(){
 		console.log("this is the create reminder function: this is scope phone_number")
-		console.log($scope.phone_number)
+		console.log($scope.phoneNumber)
+
+
+
 		var reminder_attr = {
 			"hourAndDuration": $scope.hourAndDuration,
 			"day": $scope.day,
 			"frequency": $scope.frequency,
-			"phone_number": $cookies.get('user_phone_number'),
-			"reminder_name": $scope.reminder_name
+			"phone_number": $cookies.get('userPhoneNumber'),
+			"reminder_name": $scope.reminderName
 		}
 
 		$http.post('/reminders', reminder_attr).success(function(response){
@@ -99,17 +102,20 @@ app.controller('RemindersController', ['$scope', '$http', '$cookies', function($
 		})
 		.error(function(response){
 			console.log("Failed to create_reminder")
-			console.log(response)
-			alert(response[0])
+			console.log(response[0])
+			if ($cookies.get('userPhoneNumber') == 'not logged in'){
+				alert("Login to create a reminder")
+			} else{
+				alert(response[0])			
+			}
 		})
 	}
 
 
 	$scope.signin = function(){
 		var user = {
-			"phone_number": $scope.phone_number,
+			"phone_number": $scope.phoneNumber,
 			"password": $scope.password 
-
 		}
 
 
@@ -117,10 +123,10 @@ app.controller('RemindersController', ['$scope', '$http', '$cookies', function($
 			$cookies.put('loggedin', 'true')
 			$cookies.put('user_name', response['user_name'])
 			$scope.reminders = response['reminders']
-			$scope.user_name = response['user_name']
-			$scope.phone_number = String(response['user_phone_number'])
-			$cookies.put('user_phone_number', String(response['user_phone_number']))
-			$('.welcome_message').html("Logged in as: " + $scope.user_name)
+			$scope.userName = response['user_name']
+			$scope.phoneNumber = String(response['user_phone_number'])
+			$cookies.put('userPhoneNumber', String(response['user_phone_number']))
+			$('.welcome_message').html("Logged in as: " + $scope.userName)
 			check_login();
 			get_reminders();
 			window.location.reload();
@@ -128,7 +134,7 @@ app.controller('RemindersController', ['$scope', '$http', '$cookies', function($
 		.error(function(response){
 			console.log("Failed")
 			$cookies.put('loggedin', 'false')
-			$cookies.put('user_phone_number', 'not logged in')
+			$cookies.put('userPhoneNumber', 'not logged in')
 			$('.signin-error h5').html("Incorrect phone number or password")
 			$('.signin-error').show()
 			$("#myModal").modal("toggle")
@@ -137,11 +143,10 @@ app.controller('RemindersController', ['$scope', '$http', '$cookies', function($
 
 	$scope.register = function(){
 		var user = {
-			"phone_number": $scope.phone_number,
+			"phone_number": $scope.phoneNumber,
 			"password": $scope.password,
-			"user_name": $scope.user_name
+			"user_name": $scope.userName
 		}
-
 
 		$http.post('/users', user).success(function(response){
 			console.log("Successfully Registered")
@@ -164,9 +169,9 @@ app.controller('RemindersController', ['$scope', '$http', '$cookies', function($
 
 	$scope.log_out = function(){
 		$cookies.remove('loggedin')
-		$cookies.remove('user_phone_number')
+		$cookies.remove('userPhoneNumber')
 		$cookies.remove('user_name')
-		$scope.user_name = ""
+		$scope.userName = ""
 		$('.welcome_message').css("display", "none")
 		check_login()
 	}
