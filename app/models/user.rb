cf36e9b27e7d3291ae666afea9fa53f36e601f80
grantmw <grantmwong@gmail.com>
2016-11-21 12:07:26 -0800

    error handling for non-phone numbers

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,11 @@ require 'bcrypt'
 class User < ActiveRecord::Base
 	has_many :reminders
 	validates :user_name, presence: true, length: {within: 3..12, :message => "Username must be 3-12 characters in length."}
-	validates :phone_number, presence: true, :uniqueness => {message: "Phone number already in use."}
+	validates 	:phone_number, 
+				presence: true, 
+				:uniqueness => {message: "Phone number already in use."},
+				:numericality => {message: 'Must be a valid phone number.'},
+				length: {within: 10..15, :message => "Phone number must be 10-15 numbers in length."}
 	include BCrypt
 
 	def password
